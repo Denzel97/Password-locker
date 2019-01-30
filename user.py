626d2655acbe54ab723cb6d70a2ce8f883fc79a0
Denzel97 <denzelwash97@gmail.com>
2019-01-30 14:32:16 +0300

    add password to table

--- user.py
+++ user.py
@@ -6,12 +6,13 @@ class User:
 
     user_list = [] # Empty user list
 
-    def __init__(self,first_name,last_name,number,email):
+    def __init__(self,first_name,last_name,number,email,password):
 
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = number
         self.email = email
+        self.password
 
     # Init method up here
     def save_user(self):
